cc9987f8586cf605b199605069948be112c5d8b7
egonzalezt <egonzalezt@eafit.edu.co>
2021-10-09 20:04:12 -0500

    first heroku commit

--- Flask/app.py
+++ Flask/app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 app.register_blueprint(methods)
 
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000), debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
