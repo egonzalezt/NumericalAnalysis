6a578ab4314387ec14967aaa1a9db0255ec7a651
egonzalezt <egonzalezt@eafit.edu.co>
2021-10-09 19:10:20 -0500

    modify app.py

--- Flask/app.py
+++ Flask/app.py
@@ -4,9 +4,8 @@ import os
 from Routes.routes import methods
 
 app = Flask(__name__)
-app.config["DEBUG"] = False
 app.register_blueprint(methods)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run()
+    app.run(host='0.0.0.0', port=port, debug=True)
