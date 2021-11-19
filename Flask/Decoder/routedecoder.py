6907453bf0af7f607a607fb37255256e31de268e
egonzalezt <egonzalezt@eafit.edu.co>
2021-11-19 15:31:00 -0500

    Fixed problems with decoder
    Co-Authored-By: Dcg <53051443+dfgarciac1@users.noreply.github.com>
    Co-Authored-By: xguerrerov1 <88469900+xguerrerov1@users.noreply.github.com>
    Co-Authored-By: Camilo Cañas Jaramillo <53055930+ccanasj@users.noreply.github.com>

--- Flask/Decoder/routedecoder.py
+++ Flask/Decoder/routedecoder.py
@@ -1,27 +1,38 @@
 from numpy import equal
 
 
-def decode(params:list,values:dict):
+def decode(params:dict,values:dict):
     finalvals = {}
     valid=True
     unknowParam = []
     apiValues = []
+    invalidType = []
     result = ", "
-    out_str = result.join(params)    
-    for value in values:
-        if value in params and values[value]!='':
-            finalvals[value] = values[value]
-            apiValues.append(value)
-        else:
-            unknowParam.append(value)
-            valid = False
-    if(params!=apiValues and valid):
-        problem = "Missing params!, please send the required params. Endpoint params: {params}".format(params=out_str)
-        raise ValueError(problem)
-    elif(valid):
-        return finalvals
-    else:
-        invalidParams = result.join(unknowParam) 
-        problem = "Bad params!, Unknow params: {value}. Endpoint params: {params}".format(value=invalidParams,params=out_str)
-        raise ValueError(problem)
+    stop=False
+    out_str = result.join(params)   
+    for k, v in params.items():
+        if not isinstance(values[k], v):
+            invalidType.append(k)
+            stop=True
 
+    if not stop:
+        for value in values:     
+            if value in params.keys() and values[value]!='':
+                finalvals[value] = values[value]
+                apiValues.append(value)
+            else:
+                unknowParam.append(value)
+                valid = False
+        if((not all(value in params for value in apiValues)) and valid):
+            problem = "Missing params!, please send the required params. Endpoint params: {params}".format(params=out_str)
+            raise ValueError(problem)
+        elif(valid):
+            return finalvals
+        else:
+            invalidParams = result.join(unknowParam) 
+            problem = "Bad params!, Unknow params: {value}. Endpoint params: {params}".format(value=invalidParams,params=out_str)
+            raise ValueError(problem)
+    else:
+        invalidParams = result.join(invalidType) 
+        problem = f"Invalid data type please check those values {invalidParams}"
+        raise ValueError(problem)
